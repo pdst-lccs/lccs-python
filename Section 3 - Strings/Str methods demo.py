263d240b0c27e24e4ae858048e04700b60e7e1a6
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-20 15:27:36 +0000

    Update and rename Page 80 - Str methods demo.py to Str methods demo.py

--- Section 3 - Strings/Str methods demo.py
+++ Section 3 - Strings/Str methods demo.py
@@ -1,10 +1,8 @@
-'''
 # Event: LCCS Python Fundamental Skills Workshop
 # Date: May 2018
 # Author: Joe English, PDST
 # eMail: computerscience@pdst.ie
 # Purpose: A program to demonstrate the use of common string methods
-'''
 
 pangram = "the quick brown fox jumps over the lazy dog!"
 
