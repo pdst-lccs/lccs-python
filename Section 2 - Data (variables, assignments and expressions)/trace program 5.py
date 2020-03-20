13a09add91e1eee6aa1dac879ac2afe833cdbc63
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-20 09:32:31 +0000

    Update and rename Page 32 - trace program 5.py to trace program 5.py

--- Section 2 - Data (variables, assignments and expressions)/trace program 5.py
+++ Section 2 - Data (variables, assignments and expressions)/trace program 5.py
@@ -1,10 +1,9 @@
-'''
 # Event: LCCS Python Fundamental Skills Workshop
 # Date: May 2018
 # Author: Joe English, PDST
 # eMail: computerscience@pdst.ie
 # Purpose: To demonstrate program tracing
-'''
+
 days = 2
 hrs = 24
 mins = 60
