72ad536231f4bba5c37aaafe4bfff69105b36587
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-20 19:40:09 +0000

    Update and rename Page 97 - Create List 2.py to Create List 2.py

--- Section 4 - Lists/Create List 2.py
+++ Section 4 - Lists/Create List 2.py
@@ -1,12 +1,10 @@
-'''
 # Event: LCCS Python Fundamental Skills Workshop
 # Date: May 2018
 # Author: Joe English, PDST
 # eMail: computerscience@pdst.ie
 # Purpose: A program to demonstrate how to create lists
-'''
 
-# Lists can be created with data (elements)
+# Lists can be created with data (each value is a list element)
 boysNames = ['John', 'Jim', 'Alex', 'Fred']
 girlsNames = ['Sarah', 'Alex', 'Pat', 'Mary']
 favouriteSongs = ['Moondance', 'Linger', 'Stairway to Heaven']
