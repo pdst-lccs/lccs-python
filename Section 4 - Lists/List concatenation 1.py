b0654e5b7b39a0ad7061c77cb228dfeb1a0f2581
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-20 19:41:30 +0000

    Update and rename Page 99 - List concatenation 1.py to List concatenation 1.py

--- Section 4 - Lists/List concatenation 1.py
+++ Section 4 - Lists/List concatenation 1.py
@@ -1,10 +1,8 @@
-'''
 # Event: LCCS Python Fundamental Skills Workshop
 # Date: May 2018
 # Author: Joe English, PDST
 # eMail: computerscience@pdst.ie
 # Purpose: A program to demonstrate list concatenation
-'''
 
 # Concatenate two lists and store the result in a new list
 boysNames = ['John', 'Jim', 'Alex', 'Fred']
