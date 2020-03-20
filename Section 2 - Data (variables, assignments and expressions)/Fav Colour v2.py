5bdadf7ad596acdb2c8f7248c361d56d8008f5b2
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-20 09:21:32 +0000

    Update and rename Page 24 - Fav Colour v2.py to Fav Colour v2.py

--- Section 2 - Data (variables, assignments and expressions)/Fav Colour v2.py
+++ Section 2 - Data (variables, assignments and expressions)/Fav Colour v2.py
@@ -1,10 +1,9 @@
-'''
 # Event: LCCS Python Fundamental Skills Workshop
 # Date: May 2018
 # Author: Joe English, PDST
 # eMail: computerscience@pdst.ie
 # Purpose: To demonstrate the use of variables
-'''
+
 personName = "Alex"
 favouriteColour = "red"
 print("Hi", personName, "- your favourite colour is", favouriteColour)
