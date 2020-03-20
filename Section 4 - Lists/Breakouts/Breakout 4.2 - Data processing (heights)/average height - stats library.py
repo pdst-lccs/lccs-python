015c485eadd95e84d7727a11f5fd36fcc5778541
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-20 20:58:37 +0000

    Update and rename Page 115 - average height - stats library.py to average height - stats library.py

--- Section 4 - Lists/Breakouts/Breakout 4.2 - Data processing (heights)/average height - stats library.py
+++ Section 4 - Lists/Breakouts/Breakout 4.2 - Data processing (heights)/average height - stats library.py
@@ -1,10 +1,9 @@
-'''
 # Event: LCCS Python Fundamental Skills Workshop
 # Date: May 2018
 # Author: Joe English, PDST
 # eMail: computerscience@pdst.ie
 # Purpose: A program to calculate the arithmetic mean
-'''
+
 from statistics import mean
 
 # Hard code a list of heights
