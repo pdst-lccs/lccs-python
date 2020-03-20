155c9d485a83a521f659869935886c94bc0b4cb3
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-20 08:20:27 +0000

    Add files via upload

---
+++ Section 1 - Getting Started/Esc Sequence2.py
@@ -0,0 +1,7 @@
+# Event: LCCS Python Fundamental Skills Workshop
+# Date: May 2018
+# Author: Joe English, PDST
+# eMail: computerscience@pdst.ie
+# Purpose: Program to demonstrate the use of escape sequences
+print("A\tB\nC")
+print("C:\\Users\\johndoe\\Documents\\myfile.txt")
