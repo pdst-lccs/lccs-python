db7a9eec96f6054a02e949d49e9d1ebf63d5a274
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-20 14:40:38 +0000

    Update and rename Page 71 - program 3 - concatenation.py to concatenation3.py

--- Section 3 - Strings/concatenation3.py
+++ Section 3 - Strings/concatenation3.py
@@ -1,10 +1,8 @@
-'''
 # Event: LCCS Python Fundamental Skills Workshop
 # Date: May 2018
 # Author: Joe English, PDST
 # eMail: computerscience@pdst.ie
 # Purpose: A program to demonstrate string concatenation
-'''
 
 noun = input("Enter a singular noun: ")
 print("The plural of "+noun+" is "+noun+"s")
