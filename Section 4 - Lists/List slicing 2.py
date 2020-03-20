93a5f614972e4ab8dff1265795a0899dc3a5d078
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-20 20:13:17 +0000

    Add files via upload

---
+++ Section 4 - Lists/List slicing 2.py
@@ -0,0 +1,13 @@
+# Event: LCCS Python Fundamental Skills Workshop
+# Date: May 2018
+# Author: Joe English, PDST
+# eMail: computerscience@pdst.ie
+# Purpose: A program to demonstrate list slicing
+
+# Slicing creates a new list
+fruits = ['apple', 'pear', 'orange', 'banana', 'kiwi']
+exoticFruits = fruits[2:5]
+print(exoticFruits)
+
+
+
