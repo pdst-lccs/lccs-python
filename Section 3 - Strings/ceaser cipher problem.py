befc5781aa654be98cdd3a5ba8557853048c82b8
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-20 15:18:24 +0000

    Update and rename Page 79 - ceaser cipher problem.py to ceaser cipher problem.py

--- Section 3 - Strings/ceaser cipher problem.py
+++ Section 3 - Strings/ceaser cipher problem.py
@@ -1,10 +1,8 @@
-'''
 # Event: LCCS Python Fundamental Skills Workshop
 # Date: May 2018
 # Author: Joe English, PDST
 # eMail: computerscience@pdst.ie
 # Purpose: A program to shift a single character by 1 e.g. 'A' -> 'B'
-'''
 
 inStr = input("Enter any character: ")
 outStr = chr(ord(inStr)+1)
