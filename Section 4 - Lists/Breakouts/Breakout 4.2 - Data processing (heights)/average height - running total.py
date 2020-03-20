518cc1c05fd562eb937e959dddb962ac30a37057
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-20 20:57:14 +0000

    Update and rename Page 115 - average height - running total.py to average height - running total.py

--- Section 4 - Lists/Breakouts/Breakout 4.2 - Data processing (heights)/average height - running total.py
+++ Section 4 - Lists/Breakouts/Breakout 4.2 - Data processing (heights)/average height - running total.py
@@ -1,10 +1,8 @@
-'''
 # Event: LCCS Python Fundamental Skills Workshop
 # Date: May 2018
 # Author: Joe English, PDST
 # eMail: computerscience@pdst.ie
 # Purpose: A program to calculate the average height of 5 people
-'''
 
 # A program to calculate the average height of 5 people
 # The heights are stored in a file called 'heights.txt'
