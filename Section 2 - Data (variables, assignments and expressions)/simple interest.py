a56cf1659b8841708662d3d29dd1e083c3dd4a58
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-20 11:22:18 +0000

    Update and rename Page 39 - simple interest.py to simple interest.py

--- Section 2 - Data (variables, assignments and expressions)/simple interest.py
+++ Section 2 - Data (variables, assignments and expressions)/simple interest.py
@@ -1,10 +1,8 @@
-'''
 # Event: LCCS Python Fundamental Skills Workshop
 # Date: May 2018
 # Author: Joe English, PDST
 # eMail: computerscience@pdst.ie
 # Purpose: A program to calculate simple interest
-'''
 
 # Ask the user for the principal
 principal = input("Enter principal: ")
