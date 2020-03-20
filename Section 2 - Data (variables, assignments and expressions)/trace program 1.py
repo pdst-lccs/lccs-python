355f8bd0d24aa041dc4b57fcdf8eb23fc284a107
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-20 09:28:21 +0000

    Update and rename Page 32 - trace program 1.py to trace program 1.py

--- Section 2 - Data (variables, assignments and expressions)/trace program 1.py
+++ Section 2 - Data (variables, assignments and expressions)/trace program 1.py
@@ -1,10 +1,9 @@
-'''
 # Event: LCCS Python Fundamental Skills Workshop
 # Date: May 2018
 # Author: Joe English, PDST
 # eMail: computerscience@pdst.ie
 # Purpose: To demonstrate program tracing
-'''
+
 goals = 0
 goals = goals + 1
 print("The value of goals is", goals)
