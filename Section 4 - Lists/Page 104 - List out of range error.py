bab307a81e43dc559d899cc80add6231158dad36
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-20 19:49:39 +0000

    Update Page 104 - List out of range error.py

--- Section 4 - Lists/Page 104 - List out of range error.py
+++ Section 4 - Lists/Page 104 - List out of range error.py
@@ -1,16 +1,16 @@
-'''
 # Event: LCCS Python Fundamental Skills Workshop
 # Date: May 2018
 # Author: Joe English, PDST
 # eMail: computerscience@pdst.ie
 # Purpose: A program to demonstrate a list index out of range error
-'''
 
-# This is not something you want to see happening (too often!)
+# Initialise a list of 5 fruits and try and access the 6th one
 fruits = ['Strawberry', 'Lemon', 'Orange', 'Raspberry', 'Cherry']
-print(fruits[5])
-print("Hello World") # This line will never get executed
-lifeSupport = True # Neither will this on
+print(fruits[5]) # <-- Syntax Error - out of bounds
+
+# The following lines will not be executed because Python stops when it oncounters a syntax error
+print("Hello World") 
+lifeSupport = True
 
 
 
