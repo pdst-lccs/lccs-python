3ed27ea6f453917e155c3b4c0944c2afe5ab0247
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-20 14:40:02 +0000

    Update and rename Page 71 - program 2 - concatenation.py to concatenation2.py

--- Section 3 - Strings/concatenation2.py
+++ Section 3 - Strings/concatenation2.py
@@ -1,10 +1,8 @@
-'''
 # Event: LCCS Python Fundamental Skills Workshop
 # Date: May 2018
 # Author: Joe English, PDST
 # eMail: computerscience@pdst.ie
 # Purpose: A program to demonstrate string concatenation
-'''
 
 pangram = "The quick brown fox jumps over the lazy dog!"
 #          01234567890123456789012345678901234567890123  
