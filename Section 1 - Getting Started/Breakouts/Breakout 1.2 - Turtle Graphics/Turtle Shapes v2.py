b9f8a1edc0c09d0ba984c29d9c480e50b8a399bb
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-20 08:32:26 +0000

    Add files via upload

---
+++ Section 1 - Getting Started/Breakouts/Breakout 1.2 - Turtle Graphics/Turtle Shapes v2.py
@@ -0,0 +1,42 @@
+# Event: LCCS Python Fundamental Skills Workshop
+# Date: May 2018
+# Author: Joe English, PDST
+# eMail: computerscience@pdst.ie
+# Purpose: Turtle Graphics - Further Activities
+# Match the code blocks below to the corresponding shape
+
+from turtle import * # import the turtle graphics library
+
+# BLOCK 1
+forward(100)
+right(90)
+forward(50)
+right(90)
+forward(100)
+right(90)
+forward(50)
+
+# BLOCK 2
+#forward(100)
+#left(60)
+#forward(100)
+#left(60)
+#forward(100)
+
+# BLOCK 3
+#forward(100)
+#left(120)
+#forward(100)
+#left(120)
+#forward(100)
+
+# BLOCK 4
+#forward(100)
+#left(90)
+#forward(50)
+#left(90)
+#forward(100)
+#left(90)
+#forward(50)
+
+
