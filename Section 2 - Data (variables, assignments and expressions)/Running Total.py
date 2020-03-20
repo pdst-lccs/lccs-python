ee6db6a5a4ff952c117540ae330e4aaa3a02afc1
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-20 12:00:21 +0000

    Update and rename Page 45 - Running Total.py to Running Total.py

--- Section 2 - Data (variables, assignments and expressions)/Running Total.py
+++ Section 2 - Data (variables, assignments and expressions)/Running Total.py
@@ -1,10 +1,8 @@
-'''
 # Event: LCCS Python Fundamental Skills Workshop
 # Date: May 2018
 # Author: Joe English, PDST
 # eMail: computerscience@pdst.ie
 # Purpose: A program to demonstrate how to keep a running total
-'''
 
 # Program to calculate a running total
 
