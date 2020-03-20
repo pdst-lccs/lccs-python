3bf120b20110eb0295103f1187496e46448da667
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-20 14:38:12 +0000

    Update and rename Page 71 - program 1 - concatenation.py to concatenation1.py

--- Section 3 - Strings/concatenation1.py
+++ Section 3 - Strings/concatenation1.py
@@ -1,10 +1,8 @@
-'''
 # Event: LCCS Python Fundamental Skills Workshop
 # Date: May 2018
 # Author: Joe English, PDST
 # eMail: computerscience@pdst.ie
 # Purpose: A program to demonstrate string concatenation
-'''
 
 word1 = "Leaving"
 word2 = "Certificate"
