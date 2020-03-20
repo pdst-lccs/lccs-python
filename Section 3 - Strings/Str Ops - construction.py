82537c3b1d682e00e954b88f21fc76201c5d1955
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-20 14:45:39 +0000

    Update and rename Page 72 - Str Ops - construction.py to Str Ops - construction.py

--- Section 3 - Strings/Str Ops - construction.py
+++ Section 3 - Strings/Str Ops - construction.py
@@ -1,10 +1,8 @@
-'''
 # Event: LCCS Python Fundamental Skills Workshop
 # Date: May 2018
 # Author: Joe English, PDST
 # eMail: computerscience@pdst.ie
 # Purpose: A program to demonstrate string construction
-'''
 
 # Initialise the output string
 outStr = "The quick "
