00de368c98b9ef8ed0b1b4c2c582719d84d6cfc9
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-20 14:47:18 +0000

    Update and rename Page 72 - Str Ops - multiplication.py to Str Ops - multiplication.py

--- Section 3 - Strings/Str Ops - multiplication.py
+++ Section 3 - Strings/Str Ops - multiplication.py
@@ -1,10 +1,9 @@
-'''
 # Event: LCCS Python Fundamental Skills Workshop
 # Date: May 2018
 # Author: Joe English, PDST
 # eMail: computerscience@pdst.ie
 # Purpose: A program to demonstrate string multiplication
-'''
+
 word1 = "Hello"
 print(word1*3)
 print(word1[1]*3)
