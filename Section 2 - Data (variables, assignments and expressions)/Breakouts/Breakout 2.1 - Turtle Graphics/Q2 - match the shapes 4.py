eb8e8ebc30a82ebdd408a33d547abd52d8181107
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-20 12:20:03 +0000

    Add files via upload

---
+++ Section 2 - Data (variables, assignments and expressions)/Breakouts/Breakout 2.1 - Turtle Graphics/Q2 - match the shapes 4.py
@@ -0,0 +1,39 @@
+# Event: LCCS Python Fundamental Skills Workshop
+# Date: May 2018
+# Author: Joe English, PDST
+# eMail: computerscience@pdst.ie
+# Purpose: Turtle Graphics - match the code to the shape
+
+from turtle import *
+
+# Question: Which shape does this code draw?
+x=50
+y=25
+forward(2*x+y)
+left(90)
+forward(x+2*y)
+left(90)
+forward(x+2*y)
+left(90)
+x = 2*y
+forward(x+y)
+
+# Hint: Run the code to find out!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Answer: CODE BLOCK 4 matches shape 1
+
+
