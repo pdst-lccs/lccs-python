e12a8719e5309e701a5b4ff1dd2870f3eaa36731
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-20 15:11:37 +0000

    Update and rename Page 77 - chr demo.py to chr demo.py

--- Section 3 - Strings/chr demo.py
+++ Section 3 - Strings/chr demo.py
@@ -1,10 +1,8 @@
-'''
 # Event: LCCS Python Fundamental Skills Workshop
 # Date: May 2018
 # Author: Joe English, PDST
 # eMail: computerscience@pdst.ie
 # Purpose: A program to demonstrate the chr function
-'''
 
 print(chr(65))  # 'A'
 print(chr(90))  # 'Z'
@@ -12,7 +10,7 @@ print(chr(97))  # 'a'
 print(chr(49))  # '1'
 
 '''
-# A program to demonstrate the use of the 'chr' function using a for loop
+# The code below demonstrates the use of the 'chr' function using a for loop
 # Display all characters whose ordinal codes are between 48 and 58
 for i in range(48, 58):
     print(i, chr(i))
