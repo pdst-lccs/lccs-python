6b8c8c0cdc22afef0a1b3d9ef4d38ca3d696d648
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-20 20:39:10 +0000

    Update and rename Page 108 - slicing exercise.py to slicing exercise.py

--- Section 4 - Lists/slicing exercise.py
+++ Section 4 - Lists/slicing exercise.py
@@ -1,11 +1,9 @@
-'''
 # Event: LCCS Python Fundamental Skills Workshop
 # Date: May 2018
 # Author: Joe English, PDST
 # eMail: computerscience@pdst.ie
 # Purpose: To demonstrate syntax errors relating to lists
 # To extend thinking in relation to list slicing range errors
-'''
 
 suits = ['Hearts','Diamonds','Spades','Clubs']
 cardFaces = ['Ace','1','2','3','4','5','6','7','8','9','Jack','Queen','King']
@@ -25,21 +23,23 @@ print(blackSuits)
 
 
 '''
+# Bonus (additional) code!
 # A program to generate a random card e.g. Ace of Spades
 import random
 
 suits=['Hearts','Diamonds','Spades','Clubs']
 faces=['A','1','2','3','4','5','6','7','8','9','J','Q','K',]
 
+# 4 suits so ....
+# generate a random number between 0 and 3 (inclusive)
+r1 = random.randint(0,3)
+suit = suits[r1]
 
-# pick a random number between 0 and 3 (inclusive)
-r1=random.randint(0,3)
-suit=suits[r1]
+# 13 cards so ....
+# generate a random number between 0 and 12 (inclusive)
+r2 = random.randint(0,12)
+face = faces[r2]
 
-# pick a random number between 0 and 12 (inclusive)
-r2=random.randint(0,12)
-face=faces[r2]
-
-card=face+' of '+suit
+card = face+' of '+suit
 print(card)
 '''
