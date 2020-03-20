483740f294091c70e20988ffdb7fde91f72e1d9e
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-20 19:29:20 +0000

    Update and rename Page 97 - Create List 1.py to Create List 1.py

--- Section 4 - Lists/Create List 1.py
+++ Section 4 - Lists/Create List 1.py
@@ -1,10 +1,8 @@
-'''
 # Event: LCCS Python Fundamental Skills Workshop
 # Date: May 2018
 # Author: Joe English, PDST
 # eMail: computerscience@pdst.ie
 # Purpose: A program to demonstrate how to create lists
-'''
 
 # Lists can be initially empty
 boysNames = []
