d967e1e74b8fa7b8edd27a8688b76b21c87e0f15
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-20 19:44:47 +0000

    Update and rename Page 102 - Fruit Machine.py to Fruit Machine 1.py

--- Section 4 - Lists/Fruit Machine 1.py
+++ Section 4 - Lists/Fruit Machine 1.py
@@ -1,10 +1,8 @@
-'''
 # Event: LCCS Python Fundamental Skills Workshop
 # Date: May 2018
 # Author: Joe English, PDST
 # eMail: computerscience@pdst.ie
 # Purpose: A program to simulate a fruit (slot) machine
-'''
 
 # Program to simulate a fruit machine!
 import random
