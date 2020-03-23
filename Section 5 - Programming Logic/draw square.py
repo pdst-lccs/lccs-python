0704698016852fb4d27003d7d7de0ca3d5c6e874
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-23 12:04:12 +0000

    Add files via upload

---
+++ Section 5 - Programming Logic/draw square.py
@@ -0,0 +1,16 @@
+# Event: LCCS Python Fundamental Skills Workshop
+# Date: May 2018
+# Author: Joe English, PDST
+# eMail: computerscience@pdst.ie
+# Purpose: A program to demonstrate the use of turtle graphics
+
+# import the turtle graphics library
+from turtle import *
+
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
