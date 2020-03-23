634e16a73afe5157edbab7e147b5eb7932750515
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-23 12:56:33 +0000

    Update and rename Page 179 - homework v1.py to homework v1.py

--- Section 6 - Modular Programming/homework v1.py
+++ Section 6 - Modular Programming/homework v1.py
@@ -1,15 +1,15 @@
-'''
 # Event: LCCS Python Fundamental Skills Workshop
 # Date: Dec 2018
 # Author: Joe English, PDST
 # eMail: computerscience@pdst.ie
 # Purpose: A program to display another silly poem
-'''
-def homework():
+
+def homework(): # function header
    print("Jack loves to do his homework")
    print("He never misses a day")
    print("He even loves the men in white")
    print("Who are taking him away")
+   # End of function
 
-homework()
+homework() # call the function
 print("The End!")
