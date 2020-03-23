6836caf0e144a376aefc93d4cc9ef64301e77e8e
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-23 16:38:01 +0000

    Update and rename Page 198 - isLeap.py to isLeap v3 and v4.py

--- Section 6 - Modular Programming/isLeap v3 and v4.py
+++ Section 6 - Modular Programming/isLeap v3 and v4.py
@@ -1,10 +1,9 @@
-'''
 # Event: LCCS Python Fundamental Skills Workshop
 # Date: Dec 2018
 # Author: Joe English, PDST
 # eMail: computerscience@pdst.ie
 # Purpose: Exercise on page 198 - encapsulate the code below into functions
-'''
+
 
 # Part 1
 year = int(input("Enter a year: "))
