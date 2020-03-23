a4eb1f0cf34d265c74595e3b8fb97fe463e55d53
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-23 11:26:25 +0000

    Update and rename Page 153 - simple for loop.py to simple for loop.py

--- Section 5 - Programming Logic/simple for loop.py
+++ Section 5 - Programming Logic/simple for loop.py
@@ -1,10 +1,9 @@
-'''
 # Event: LCCS Python Fundamental Skills Workshop
 # Date: May 2018
 # Author: Joe English, PDST
 # eMail: computerscience@pdst.ie
 # Purpose: A program to demonstrate a for loop
-'''
+
 # Simple for loop
 
 for counter in range(10):
