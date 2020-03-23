b356acfe13ecb833d2a935e2d677247da4db90e5
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-23 16:18:51 +0000

    Update and rename Page 189 - subtract.py to subtract.py

---
+++ Section 6 - Modular Programming/subtract.py
@@ -0,0 +1,20 @@
+# Event: LCCS Python Fundamental Skills Workshop
+# Date: Dec 2018
+# Author: Joe English, PDST
+# eMail: computerscience@pdst.ie
+# Purpose: A program to demonstrate function composition
+
+def sub(a, b):
+    answer = a - b
+    return answer
+
+print( sub( 2, sub(3, 4))) # -> displays 3
+print( sub( sub(2, 3), 4)) # -> displays -5
+print( sub( 2, sub(3, sub(4, 5)))) # -> displays -2
+
+# Predict what the print statements below would display
+# Now uncomment the lines and run the code
+# print( sub( 2, sub(3, 4)))
+# print( sub( 2, sub(3, sub(4, 5))))
+
+# Were your predictions correct?
