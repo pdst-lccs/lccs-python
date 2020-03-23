17ce9c168c90a98e3b510f6ca467c950c4187efa
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-23 16:24:33 +0000

    Update and rename Page 191 - future value.py to future value.py

--- Section 6 - Modular Programming/future value.py
+++ Section 6 - Modular Programming/future value.py
@@ -1,10 +1,8 @@
-'''
 # Event: LCCS Python Fundamental Skills Workshop
 # Date: Dec 2018
 # Author: Joe English, PDST
 # eMail: computerscience@pdst.ie
 # Purpose: A program to perform a compound interest calculation
-'''
 
 # Calculates future value 
 def future_value(principal, rate, time): 
