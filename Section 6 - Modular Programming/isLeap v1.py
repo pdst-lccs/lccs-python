9d23150815564f6969d3425bcee0acf6c7105456
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-23 16:33:27 +0000

    Update and rename Page 196 - isLeap.py to isLeap v1.py

--- Section 6 - Modular Programming/isLeap v1.py
+++ Section 6 - Modular Programming/isLeap v1.py
@@ -1,10 +1,9 @@
-'''
 # Event: LCCS Python Fundamental Skills Workshop
 # Date: Dec 2018
 # Author: Joe English, PDST
 # eMail: computerscience@pdst.ie
 # Purpose: A program to determine whether a year is leap or not
-'''
+
 def isLeap(n):
     if n % 400 == 0:
         return True
