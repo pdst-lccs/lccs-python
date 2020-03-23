d3f80fcb88b2ef861518785dbaa8088d45754103
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-23 16:28:58 +0000

    Update and rename Page 194 - even and odd.py to even and odd.py

--- Section 6 - Modular Programming/even and odd.py
+++ Section 6 - Modular Programming/even and odd.py
@@ -1,10 +1,8 @@
-'''
 # Event: LCCS Python Fundamental Skills Workshop
 # Date: Dec 2018
 # Author: Joe English, PDST
 # eMail: computerscience@pdst.ie
 # Purpose: A program to demonstrate Boolean functions
-'''
 
 # A function to determine evenness
 def isEven(number):
@@ -13,6 +11,12 @@ def isEven(number):
     else:
         return False
 
+# display even numbers < 100
+for i in range (100):
+    if isEven(i):
+        print(i)
+
+'''
 # A function to determine oddness
 def isOdd(number):
     return not isEven(number)
@@ -20,3 +24,4 @@ def isOdd(number):
 for i in range (100):
     if isOdd(i):
         print(i)
+'''
