a2931f04db1ffa934d75569798d6e9cf4904d513
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-23 16:09:58 +0000

    Add files via upload

---
+++ Section 6 - Modular Programming/sumOfN v1.py
@@ -0,0 +1,19 @@
+# Event: LCCS Python Fundamental Skills Workshop
+# Date: Dec 2018
+# Author: Joe English, PDST
+# eMail: computerscience@pdst.ie
+# Purpose: A program to demonstrate return values
+
+# Add all the numbers from 0 to n
+def sumOfN(n):
+    total = 0
+    
+    for i in range(n+1):
+        total = total + i
+
+    return total
+
+# Call the function
+sumOfN(10)
+
+# Explain why the program does not display any output
