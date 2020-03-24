de4cd34485cb33af30dd0f2a0f31b76d95d97fcb
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-24 22:54:47 +0000

    Add files via upload

---
+++ Section 7 - Dictionaries/del v2.py
@@ -0,0 +1,16 @@
+# Event: LCCS Python Fundamental Skills Workshop
+# Date: Dec 2018
+# Author: Joe English, PDST
+# eMail: computerscience@pdst.ie
+# Purpose: A program to demonstrate how to clear a dictionary
+
+d = {
+    "OMG" : "Oh My God!",
+    "LOL" : "Laugh out Loud",
+    "IMHO" : "In My Humble Opinion",
+}
+
+del d
+print(d)
+
+
