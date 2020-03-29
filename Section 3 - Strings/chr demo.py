abfceda56346a029e1c3baf1223fc060d8655389
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-29 11:09:32 +0100

    Update chr demo.py

--- Section 3 - Strings/chr demo.py
+++ Section 3 - Strings/chr demo.py
@@ -9,11 +9,4 @@ print(chr(90))  # 'Z'
 print(chr(97))  # 'a'
 print(chr(49))  # '1'
 
-'''
-# The code below demonstrates the use of the 'chr' function using a for loop
-# Display all characters whose ordinal codes are between 48 and 58
-for i in range(48, 58):
-    print(i, chr(i))
-
-'''
 
