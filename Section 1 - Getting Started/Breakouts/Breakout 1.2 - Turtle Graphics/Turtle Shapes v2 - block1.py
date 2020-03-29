b996a093de9f8a9c7e7c534eff7ad13fe036a58e
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-30 00:40:11 +0100

    Add files via upload

---
+++ Section 1 - Getting Started/Breakouts/Breakout 1.2 - Turtle Graphics/Turtle Shapes v2 - block1.py
@@ -0,0 +1,16 @@
+# Event: LCCS Python Fundamental Skills Workshop
+# Date: May 2018
+# Author: Joe English, PDST
+# eMail: computerscience@pdst.ie
+# Purpose: Turtle Graphics - Further Activities
+# Match the code blocks below to the corresponding shape
+
+from turtle import * # import the turtle graphics library
+
+forward(100)
+right(90)
+forward(50)
+right(90)
+forward(100)
+right(90)
+forward(50)
---
+++ Section 1 - Getting Started/Breakouts/Breakout 1.2 - Turtle Graphics/Turtle Shapes v2 - block2.py
@@ -0,0 +1,14 @@
+# Event: LCCS Python Fundamental Skills Workshop
+# Date: May 2018
+# Author: Joe English, PDST
+# eMail: computerscience@pdst.ie
+# Purpose: Turtle Graphics - Further Activities
+# Match the code blocks below to the corresponding shape
+
+from turtle import * # import the turtle graphics library
+
+forward(100)
+left(60)
+forward(100)
+left(60)
+forward(100)
