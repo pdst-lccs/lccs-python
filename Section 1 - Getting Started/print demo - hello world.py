d1da254cdaed132b57fb3e5fc269307141b128cf
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2020-03-29 22:06:50 +0100

    Update print demo - hello world.py

--- Section 1 - Getting Started/print demo - hello world.py
+++ Section 1 - Getting Started/print demo - hello world.py
@@ -1,7 +1,1 @@
-# Event: LCCS Python Fundamental Skills Workshop
-# Date: May 2018
-# Author: Joe English, PDST
-# eMail: computerscience@pdst.ie
-# Purpose: A first Python program
 print("Hello World")
-
