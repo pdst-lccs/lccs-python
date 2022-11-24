2c7d038fe1dfc0b879757f844287913302828240
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2022-11-24 10:46:26 +0000

    Create isEven.py

---
+++ Section 6 - Modular Programming/isEven.py
@@ -0,0 +1,17 @@
+# Event: LCCS Python Fundamental Skills Workshop
+# Date: Nov 2022
+# Author: Sinead Crotty, PDST
+# eMail: computerscience@pdst.ie
+# Purpose: A program to demonstrate Boolean functions
+
+# A function to determine evenness
+def isEven(number):
+    if (number % 2 == 0):
+        return True
+    else:
+        return False
+
+# display even numbers < 100
+# for i in range (100):
+#     if isEven(i):
+#         print(i)
