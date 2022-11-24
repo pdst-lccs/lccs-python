e8e900ddb1003d31dd4152ce62fa3b59e73d42bf
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2022-11-24 11:05:40 +0000

    Create passwordCheckerSolution2.py

---
+++ Section 6 - Modular Programming/Programming Exercises 6.3/passwordCheckerSolution2.py
@@ -0,0 +1,39 @@
+# Event: LCCS Python Fundamental Skills Workshop
+# Date: Dec 2018
+# Author: Joe English, PDST
+# eMail: computerscience@pdst.ie
+# Solution: Programming Exercises 6.3 Q4
+# Purpose: A program to validate a password
+# A valid password is one which is ....
+# a) between 9 and 12 characters in length and
+# b) contains only uppercase letters/lowercase letters/digits/symbols
+
+# A function the returns True if 'userPassword' is a valid password. False otherwise
+def checkLength(word):
+    if 9 <= len(word) <= 12:
+        return True
+    else:
+        return False
+
+def checkChars(password):
+    lower_case_letters = "abcdefghijklmnopqrstuvwxyz"
+    upper_case_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    digits = "0123456789"
+    symbols = "_+-*/!?&@^"
+    acceptableChars = lower_case_letters + upper_case_letters + digits + symbols
+    result = True
+    for i in range(len(password)):
+        if password[i] not in acceptableChars:
+            result = False
+    return result
+
+def isValid(passwordToCheck):
+    lenOkay = checkLength(passwordToCheck)
+    charsOkay = checkChars(passwordToCheck)
+    return lenOkay and charsOkay
+    
+userPassword = input("Enter your password here >>>")
+if isValid(userPassword):
+    print("Valid password")
+else:
+    print("Invalid password")
