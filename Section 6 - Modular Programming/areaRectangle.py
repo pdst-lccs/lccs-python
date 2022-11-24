3de3680dada6a659b71ee1aa18b7828464b0667b
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2022-11-24 10:21:39 +0000

    Create areaRectangle.py

---
+++ Section 6 - Modular Programming/areaRectangle.py
@@ -0,0 +1,26 @@
+# Event: LCCS Python Fundamental Skills Workshop
+# Date: Nov 2022
+# Author: Sinead Crotty, PDST
+# eMail: computerscience@pdst.ie
+# Name: AreaRectangle.py
+# Purpose: A program to calculate the area of a rectangle
+
+def calc_area_rect(length, width):
+    area = length * width
+    print("The area of the rectangle is",area)
+    
+calc_area_rect(4,3)
+
+"""
+# Alternative solution
+def calcArea(length, width):
+    area = (length*width)
+    print("The area is ", area, "cm squared")
+
+
+length = int(input("Enter the length: "))
+
+width = int(input("Enter the width: "))
+
+calcArea(length, width)
+"""
