1b3f5a965672259ec3818244ce2a4cb85b2ae73c
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2023-01-05 19:30:00 +0000

    Add files via upload

---
+++ Section 7 - Dictionaries/nestedDictionaryAcronyms.py
@@ -0,0 +1,26 @@
+#Irene Stone
+#Jan 2022
+#Nested Dictionary - Acronyms
+
+acronyms={'Irish':{'GRMA': 'Go Raibh Maith Agat',
+                   'OMD': 'Ó Mo Dhia',
+                   'GOA': 'Gáire Ós Árd',
+                   'MGL': 'Maith Go Leor'},
+          'English': {'OMG': 'Oh My God!',
+                      'LOL':'Laugh out Loud',
+                      'IMHO':'In My Humble Opinion'}
+          }
+
+print(acronyms)
+
+acronyms={'Irish':{'GRMA': 'Go Raibh Maith Agat',
+                   'OMD': 'Ó Mo Dhia',
+                   'GOA': 'Gáire Ós Árd'},
+          'English': {'OMG': 'Oh My God!',
+                      'LOL':'Laugh out Loud',
+                      'IMHO':'In My Humble Opinion'},
+          'German':{'USW': 'Und So Weiter',
+                    'ZB': 'Zum Beispel'}
+          }
+
+print(acronyms)
