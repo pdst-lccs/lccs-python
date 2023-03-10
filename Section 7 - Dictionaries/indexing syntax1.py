deb948707776c405de3b1ae56e52181fd2eca689
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2023-03-10 15:16:11 +0000

    Update indexing syntax1.py

--- Section 7 - Dictionaries/indexing syntax1.py
+++ Section 7 - Dictionaries/indexing syntax1.py
@@ -2,11 +2,11 @@
 # March 2023
 # A program to demonstrate dictionary indexing - errors
 
-d = dict( {
+d = {
     "OMG" : "Oh My God!",
     "LOL" : "Laugh out Loud",
     "IMHO" : "In My Humble Opinion",
-})
+}
 
 print(d['LOLL']) # syntax error: Key does not exist
 
