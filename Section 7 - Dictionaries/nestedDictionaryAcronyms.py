646935e05f16a0a0780e53a1838edfa63f7a216a
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2023-03-10 15:13:23 +0000

    Update nestedDictionaryAcronyms.py

--- Section 7 - Dictionaries/nestedDictionaryAcronyms.py
+++ Section 7 - Dictionaries/nestedDictionaryAcronyms.py
@@ -2,13 +2,13 @@
 #Jan 2022
 #Nested Dictionary - Acronyms
 
-acronyms={'Irish':{'GRMA': 'Go Raibh Maith Agat',
+acronyms={'Gaeilge':{'GRMA': 'Go Raibh Maith Agat',
                    'OMD': 'Ó Mo Dhia',
                    'GOA': 'Gáire Ós Árd'},
           'English': {'OMG': 'Oh My God!',
                       'LOL':'Laugh out Loud',
                       'IMHO':'In My Humble Opinion'},
-          'German':{'USW': 'Und So Weiter',
+          'Deutsch':{'USW': 'Und So Weiter',
                     'ZB': 'Zum Beispel'}
           }
 
