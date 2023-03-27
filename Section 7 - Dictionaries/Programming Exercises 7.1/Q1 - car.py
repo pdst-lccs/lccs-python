dd576789e4b451d2024a28d008906b72d73d8c9f
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2023-03-27 20:19:43 +0100

    Create Q1 - car.py

---
+++ Section 7 - Dictionaries/Programming Exercises 7.1/Q1 - car.py
@@ -0,0 +1,19 @@
+# Event: LCCS Python Fundamental Skills Evening Workshop
+# Date: Mar 2023
+# Author: PDST Computer Science
+# eMail: computerscience@pdst.ie
+# Purpose: Dictionary Programming Exercises 7.1, Python manual (page 263)
+# Q1 starter code
+
+
+car = {
+    'reg': "131 CN 6439",
+    'make': "Audi",
+    'model' : "A6",
+    'year' : 2013,
+    'kms' : 52739,
+    'colour': "Silver",
+    'diesel': True,
+}
+
+print(car)
