e6153f2fb6b12e8fd7d2c54d54f950ba1537b1ea
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2023-05-19 15:34:25 +0100

    Create Q4 - Decimal Heights.py

---
+++ Section 2 - Data (variables, assignments and expressions)/Breakouts/Breakout 2.3 - Data Processing (heights)/Q4 - Decimal Heights.py
@@ -0,0 +1,22 @@
+# Event: LCCS Python Fundamental Skills Workshop
+# Date: May 2023
+# Author: Computer Science, PDST
+# eMail: computerscience@pdst.ie
+# Purpose: A program to calculate the average height of 5 people
+# Version: Question 4
+
+print("Average height calculator")
+print("=========================")
+
+# Read in the 5 values
+h1 = float(input("Enter first height (cm): "))
+h2 = float(input("Enter second height (cm): "))
+h3 = float(input("Enter third height (cm): "))
+h4 = float(input("Enter fourth height (cm): "))
+h5 = float(input("Enter fifth height (cm): "))
+
+# Calculate the average height
+avgHeigth = (h1+h2+h3+h4+h5)/5
+
+# Display the result
+print("The average height is ", round(avgHeigth,2), "cm")
