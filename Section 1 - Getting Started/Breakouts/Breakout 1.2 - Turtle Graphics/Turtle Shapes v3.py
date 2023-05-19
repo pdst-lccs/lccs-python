69c6561e5d75f774e02a7fd14452c86ad9643a7a
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2023-05-19 14:47:54 +0100

    Create Turtle Shapes v3.py

---
+++ Section 1 - Getting Started/Breakouts/Breakout 1.2 - Turtle Graphics/Turtle Shapes v3.py
@@ -0,0 +1,36 @@
+from turtle import * # import the turtle graphics library
+
+#draws a 50 x 50 square
+# forward(50)
+# left(90)
+# forward(50)
+# left(90)
+# forward(50)
+# left(90)
+# forward(50)
+
+#draws a 50 x 100 red rectangle
+# color("red") # set the pen colour to red
+# forward(50)
+# left(90)
+# forward(100)
+# left(90)
+# forward(50)
+# left(90)
+# forward(100)
+
+#draws a vertical blue line of length 100 units and thickness 5 units
+# color("blue")
+# pensize(5)
+# left(90)
+# forward(100)
+
+#draws the letter 'T' in red - pen is hidden
+color("red")
+left(90)
+forward(100)
+left(90)
+forward(50)
+left(180)
+forward(100)
+hideturtle()
